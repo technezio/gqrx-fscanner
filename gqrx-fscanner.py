39f3732bd2d751b417013cbd0d62f4b91c90a653
Luca Mattii <mattiiluca@gmail.com>
2019-04-28 23:29:47 +0200

    Comments

--- gqrx-fscanner.py
+++ gqrx-fscanner.py
@@ -7,6 +7,9 @@
 from time import sleep
 import telnetlib
 
+HOST = "127.0.0.1"
+PORT = 7356
+
 # Send a command via telnet and return the answer
 def send_cmd(s):
 	cmd = s + '\n'
@@ -29,28 +32,28 @@ TIME_SLOT = 0.3		# seconds between skips
 with open("freq.txt", "rt") as f:
 	FREQ = f.read().splitlines()
 
-tn = telnetlib.Telnet("127.0.0.1", 7356)
+tn = telnetlib.Telnet(HOST, PORT)
 
 
 while(1):
-	sql = send_cmd("l SQL")
+	sql = send_cmd("l SQL")			# read SQUELCH level
 	try:
 		sql = float(sql)
 	except:
-		sql = 0
-		print("Error. sql=", sql)
+		sql = 0		# in case of misreadings, squelch HIGH
+		print("Error. sql=", sql)	# DEBUG
 	
-	sig = send_cmd("l STRENGTH")
+	sig = send_cmd("l STRENGTH")	# read SIGNAL strength
 	try:
 		sig = float(sig)
 	except:
-		sig = -100
-		print("Error. sig=", sig)
+		sig = -100	# in case of misreadings, signal LOW
+		print("Error. sig=", sig)	# DEBUG
 	
 	# if signal strength < squelch level: skip to next FREQ
 	if sig <= sql:
 		c = "F " + FREQ[index]
-		index = (index + 1) % len(FREQ)
+		index = (index + 1) % len(FREQ)		# circular list
 		send_cmd(c)
 	
 	sleep(TIME_SLOT)
