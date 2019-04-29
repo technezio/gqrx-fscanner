60f13f29591b48a76e582f9ca439b42c0d519961
giuliof <giuliofieramosca@gmail.com>
2019-04-29 12:38:30 +0200

    Parse gqrx bookmark file

--- gqrx-fscanner.py
+++ gqrx-fscanner.py
@@ -6,6 +6,8 @@
 
 from time import sleep
 import telnetlib
+import csv
+import sys,os
 
 HOST = "127.0.0.1"
 PORT = 7356
@@ -29,8 +31,42 @@ FREQ = []
 index = 0
 TIME_SLOT = 0.3		# seconds between skips
 
-with open("freq.txt", "rt") as f:
-	FREQ = f.read().splitlines()
+if   len(sys.argv) == 1:
+	with open("freq.txt", "rt") as f:
+		FREQ = f.read().splitlines()
+elif len(sys.argv) == 3:
+	try:
+		lo_freq = int(sys.argv[1])
+		hi_freq = int(sys.argv[2])
+	except:
+		print("Your argument is invalid!")
+		sys.exit(-1)
+	
+	# Open bookmark
+	with open(os.path.expanduser("~")+'/.config/gqrx/bookmarks.csv') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=';')
+		for row in csv_reader:
+			if len(row) == 0:
+				continue
+			if row[0][0] == '#':
+				row[0] = row[0][1:]
+				labels = [i.strip() for i in row]
+			elif labels != None and len(labels) == len(row):
+				if len(row) == 6:
+					row = [i.strip() for i in row]
+					row = dict(zip(labels,row))
+					if int(row['Frequency']) < hi_freq and int(row['Frequency']) > lo_freq:
+						FREQ.append(row['Frequency'])
+					#elif len(labels) == 2:
+else:
+	print("Usage:")
+	print("Read from gqrx bookmarks: ./script low_freq high_freq")
+	print("Read from freq.txt      : ./script\n")
+	sys.exit(-1)
+
+if len(FREQ) == 0:
+    print("No frequencies given")
+    sys.exit(1)
 
 tn = telnetlib.Telnet(HOST, PORT)
 
